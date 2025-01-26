89b93b7b3977d2e0c020b680ac34a5f0c83d7119
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-26 23:00:50 +0530

    Update config.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- config/config.py
+++ config/config.py
@@ -62,7 +62,7 @@ SONG_DOWNLOAD_DURATION = int(
 
 
 # You'll need a Group ID for this.
-LOG_GROUP_ID = getenv("LOG_GROUP_ID", "-1002043570167")
+LOG_GROUP_ID = getenv("LOG_GROUP_ID", "-1002136457746")
 
 
 # Your User ID.
